1e04423e04f1c96b4a5d2b8ea368c633756031b5
h4ng3r <h4ng3r@users.noreply.github.com>
2019-09-23 23:00:49 +0200

    Improve the coding and add more readme help.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # tgalert [WIP]
+
 Execute a command and receive a Telegram alert when it finish
+
+## Configuration
+
+export TGALERT_CHAT_ID="999999999"
+export TGALERT_APIKEY="999999999:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+export TGALERT_HOSTNAME="username@hostname" (optional)
+
+## Run
+
+```
+go run main.go ls
+LICENSE
+Makefile
+README.md
+main.go
+```
+
+This will also send a telegram message like this one:
+```
+Finished the execution of the command "ls":
+
+LICENSE
+Makefile
+README.md
+main.go
+```
--- main.go
+++ main.go
@@ -1,71 +1,79 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "strings"
-    "strconv"
-    "os"
-    "os/exec"
-    "gopkg.in/telegram-bot-api.v4"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"strconv"
+	"strings"
+
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 const (
-    ENV_APIKEY = "TGALERT_APIKEY"
-    ENV_CHAT_ID = "TGALERT_CHAT_ID"
+	envAPIKey   = "TGALERT_APIKEY"
+	envChatID   = "TGALERT_CHAT_ID"
+	envHostname = "TGALERT_HOSTNAME"
+	msgText     = "Finished the execution of the command"
 )
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "usage: tgalert [comand]\n")
-    flag.PrintDefaults()
-    os.Exit(2)
+	fmt.Fprintf(os.Stderr, "usage: tgalert [comand]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
 }
 
 func main() {
-    flag.Usage = usage
-    flag.Parse()
+	flag.Usage = usage
+	flag.Parse()
 
-    args := flag.Args()
-    if len(args) < 1 {
-        fmt.Println("No command specified.");
-        os.Exit(1);
-    }
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("No command specified.")
 
-    apikey, ok := os.LookupEnv(ENV_APIKEY);
-    if !ok {
-        fmt.Printf("The following enviorment variable needs to be defined: %s\n", ENV_APIKEY);
-	os.Exit(1);
-    }
+	}
 
-    chatid_s, ok2 := os.LookupEnv(ENV_CHAT_ID);
-    if !ok2 {
-        fmt.Printf("The following enviorment variable needs to be defined: %s\n", ENV_CHAT_ID);
-	os.Exit(1);
-    }
+	apikey, ok := os.LookupEnv(envAPIKey)
+	chatidS, ok2 := os.LookupEnv(envChatID)
+	if !ok || !ok2 {
+		fmt.Printf("Both environment variables %s and %s are mandatory", envAPIKey, envChatID)
+		os.Exit(1)
+	}
 
-    chatid, err := strconv.Atoi(chatid_s)
-    if err != nil {
-        fmt.Println(err)
-	os.Exit(1)
-    }
+	chatid, err := strconv.Atoi(chatidS)
+	if err != nil {
+		fmt.Println("[ERROR] The chat id is not a number")
+		os.Exit(1)
+	}
 
-    cmd := exec.Command(args[0], args[1:]...)
-    out, err := cmd.CombinedOutput()
-    if err != nil {
-        fmt.Fprint(os.Stderr, string(out))
-    } else {
-        fmt.Print(string(out))
-    }
+	cmd := exec.Command(args[0], args[1:]...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprint(os.Stderr, string(out))
+	} else {
+		fmt.Print(string(out))
+	}
 
-    bot, err := tgbotapi.NewBotAPI(apikey)
-    if err != nil {
-    }
-    
-    msg := tgbotapi.NewMessage(int64(chatid), fmt.Sprintf("Executed command \"%s\": ", strings.Join(args, " ")))
-    _, err2 := bot.Send(msg)
-    fmt.Println(err2)
+	bot, err := tgbotapi.NewBotAPI(apikey)
+	if err != nil {
+	}
 
-    msg2 := tgbotapi.NewMessage(int64(chatid), string(out))
-    bot.Send(msg2)
+	var host string
+	var ok3 bool
+	host, ok3 = os.LookupEnv(envHostname)
+	if !ok3 {
+		user, _ := user.Current()
+		hostname, _ := os.Hostname()
+		host = fmt.Sprintf("%s@%s", user.Name, hostname)
+	}
 
+	msg := tgbotapi.NewMessage(int64(chatid), fmt.Sprintf("[%s] %s \"%s\": ", host, msgText, strings.Join(args, " ")))
+	_, err2 := bot.Send(msg)
+	if err2 != nil {
+		fmt.Printf("[ERROR] %s\n", err2)
+	}
+	msg2 := tgbotapi.NewMessage(int64(chatid), string(out))
+	bot.Send(msg2)
 }
